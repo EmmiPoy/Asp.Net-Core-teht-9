774593e1f0e0a574b84bf2d85c6a074039e96d50
Emmi Pöyhönen <Emmi.Poyhonen@edu.savonia.fi>
2020-09-14 13:22:38 +0300

    First commit

---
+++ Oppimistehtava9/Oppimistehtava9/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Oppimistehtava9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
